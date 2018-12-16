86d04dae6cb5ebfb237775b962e09ea007f0e9ed
Ph055a <ph055a@protonmail.com>
2018-12-16 10:36:34 -0500

    Changed chromium-browser package

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sudo apt update
-sudo apt-get install build-essential patch ruby-dev zlib1g-dev liblzma-dev chromium chromium-driver firefox
+sudo apt-get install build-essential patch ruby-dev zlib1g-dev liblzma-dev chromium-browser chromium-driver firefox
 
 sudo gem install solargraph faker nokogiri page-object cucumber xpath csv selenium-webdriver watir gherkin webdrivers watir-ng watir-nokogiri factory_girl activerecord database_cleaner sqlite3
 
