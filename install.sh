36dfa42a3ac9b2d3c7ff45823b9af321bcff8f49
Ph055a <ph055a@protonmail.com>
2019-07-16 20:05:56 +0700

    fixed nokogiri install

--- install.sh
+++ install.sh
@@ -10,8 +10,9 @@ chromium-chromedriver \
 firefox \
 libsqlite3-dev
 
-sudo gem install solargraph faker nokogiri page-object cucumber xpath csv selenium-webdriver watir gherkin webdrivers watir-ng watir-nokogiri factory_girl activerecord database_cleaner rake sqlite3
+sudo gem install solargraph faker page-object cucumber xpath csv selenium-webdriver watir gherkin webdrivers watir-ng watir-nokogiri factory_girl activerecord database_cleaner rake sqlite3
 
+sudo gem install nokogiri -- -use-system-libraries
 # Gekodriver (optional)
 cd ~/Downloads
 wget https://github.com/mozilla/geckodriver/releases/download/v0.24.0/geckodriver-v0.24.0-linux64.tar.gz
